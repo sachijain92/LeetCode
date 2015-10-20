9e0477baf8892247c0e4e4bf63c5b07544602c17
Sachi Jain <sachijai@buffalo.edu>
2015-10-20 04:06:29 -0500

    Update MoveZeroes.java

--- MoveZeroes.java
+++ MoveZeroes.java
@@ -29,7 +29,7 @@ public class Solution
                 else	
                  {
                   	i++;
-                    j++;
+                        j++;
                  }
                 
           }
